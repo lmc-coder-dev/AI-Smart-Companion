662d58b9f38b7594f1b5526182b9314032dc34b8
LiMingCong <2058615288@qq.com>
2026-07-21 13:35:57 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# 设置页面的配置项
+
+st.set_page_config(
+    page_title="AI智能伴侣",
+    page_icon="🤖",
+    # 布局
+    layout="wide",
+    # 控制的是侧边栏的状态
+    initial_sidebar_state="expanded",
+    menu_items={}
+)
+
+st.title("AI智能伴侣")
